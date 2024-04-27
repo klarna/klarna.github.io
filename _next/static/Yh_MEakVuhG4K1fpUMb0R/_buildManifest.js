8f217ffdc75cc622413d231819f5c66180ee6a47
Deploybot <deploy.klarna.github.io@klarna.com>
2024-04-27 00:27:28 +0000

    Build

---
+++ _next/static/Yh_MEakVuhG4K1fpUMb0R/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F180a36e03b5ac3f34fc4.css","static\u002Fchunks\u002Fpages\u002Findex-f53a3a4dae01f6d2cdcb.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-e2ffa4674eea62b50f3f.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
